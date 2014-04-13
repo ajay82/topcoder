12c6e451389839c4924362de187ac4bc4f2f6db5
Ajay Ramamurthy <ajay82@gmail.com>
2014-04-13 14:26:07 -0700

    First checkin of top coder class

---
+++ srm/ColorfulCoinsEasy.java
@@ -0,0 +1,8 @@
+//Question: http://community.topcoder.com/stat?c=problem_statement&pm=13094&rd=15849
+//Answer: http://apps.topcoder.com/wiki/display/tc/SRM+616
+
+public class ColorfulCoinsEasy{
+    public static void main(String args[]){
+	System.out.println("ColorfulCoinEasy..");
+    }
+}
